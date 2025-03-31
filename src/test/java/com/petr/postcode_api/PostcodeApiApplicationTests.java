7132360beed211eb118309255e072abf8a542646
petrdragonman <petrdragonman@me.com>
2025-03-31 12:23:18 +1100

    first commit

---
+++ src/test/java/com/petr/postcode_api/PostcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petr.postcode_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
